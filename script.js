5448edab477f92aaf61e2c4cc02460cc45c57665
EC2 Default User <ec2-user@ip-172-31-45-231.us-west-2.compute.internal>
2018-10-19 00:51:26 +0000

    Trying to implement Angular API Call

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-angular.module('script', [])
-    .controller('mainCtrl', mainCtrl);
+var app = angular.module('script', [])
+    app.controller('mainCtrl', mainCtrl);
+    app.directive('output', outputdirective);
     
-    function mainCtrl($scope) {
+    function mainCtrl ($scope, $http) {
+        $scope.songs = [];
         
-        
+        $scope.songlyrics = function(form) { 
+            var url = 'https://api.lyrics.ovh/v1/' + form.artist + '/' + form.title; 
+            $http.get(url).then(function (response) {
+                $scope.songs = response.data;
+            });
+        };
+        $scope.songlyrics();
+  }
+
+    function outputdirective () {
+        return {
+            scope: {
+                form: '='    
+            },
+            restrict: 'E',
+            replace: 'true',
+            template: (
+                '<div class = "Lyrics">' +
+                '<p>{{song.lyrics}}<p>' +
+                '</div>'
+            )
+        };
     }
