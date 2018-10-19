68913843bd16d6a7ec45ec4436529c6e66f23a7a
James Ashdown <james.t.ashdown@gmail.com>
2018-10-18 19:06:02 -0600

    added lyrics API

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-angular.module('script', [])
-    .controller('mainCtrl', mainCtrl);
-    
-    function mainCtrl($scope) {
-        
-        
-    }
+/* global angular */
+
+angular.module('lyricsLookup', [])
+  .controller(
+		'mainCtrl',
+		function ($scope, $http) {
+
+			$scope.getLyrics = function (info) {
+        let url = `https://api.lyrics.ovh/v1/${info.artist}/${info.song}`;
+        $http.get(url).then(
+          function successCallback(response) {
+            console.log(response);
+            $scope.lyrics = response.data.lyrics;
+          },
+          function errorCallback(response) {
+            console.log(response);
+          },
+        );
+			};
+		},
+	);
